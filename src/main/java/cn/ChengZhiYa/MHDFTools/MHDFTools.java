4260f2e07bf680f655b714de4e938130de7d7a49
ChengZhiYa <292200693@qq.com>
2024-03-15 19:18:52 +0800

    新增 导入CMI数据 以及HuskHomes的更多存储方式

--- src/main/java/cn/ChengZhiYa/MHDFTools/MHDFTools.java
+++ src/main/java/cn/ChengZhiYa/MHDFTools/MHDFTools.java
@@ -54,7 +54,7 @@ import static cn.chengzhiya.mhdfpluginapi.Util.ColorLog;
 import static cn.chengzhiya.mhdfpluginapi.YamlFileUtil.SaveResource;
 
 public final class MHDFTools extends JavaPlugin implements Listener {
-    public static final String Version = "1.4.0";
+    public static final String Version = "1.4.1";
     public static MHDFTools instance;
     public static boolean PAPI = true;
     public static boolean PLIB = true;
