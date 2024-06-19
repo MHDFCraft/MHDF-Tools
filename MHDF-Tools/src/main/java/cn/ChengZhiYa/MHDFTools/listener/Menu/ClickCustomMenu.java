d12347492ab43c044240f0a351356dcbc50d5da3
Dg32z_ <a1410521748@hotmail.com>
2024-06-19 13:11:34 +0800

    meneFileName -> menuFileName

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/listener/Menu/ClickCustomMenu.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/listener/Menu/ClickCustomMenu.java
@@ -18,23 +18,23 @@ public final class ClickCustomMenu implements Listener {
                 event.setCancelled(true);
                 if (getMenuFromItem(event.getCurrentItem()) != null && getItemNameFromItem(event.getCurrentItem()) != null) {
                     Player player = (Player) event.getWhoClicked();
-                    String meneFileName = getMenuFromItem(event.getCurrentItem());
+                    String menuFileName = getMenuFromItem(event.getCurrentItem());
                     String item = getItemNameFromItem(event.getCurrentItem());
 
                     if (event.getClick() == ClickType.LEFT && event.getClick() == ClickType.RIGHT) {
-                        List<String> DenyActionList = ifAllowClick(player, meneFileName, item, false);
+                        List<String> DenyActionList = ifAllowClick(player, menuFileName, item, false);
                         if (DenyActionList.isEmpty()) {
-                            runAction(player, meneFileName, getMenu(meneFileName).getStringList("Menu.ItemList." + item + ".ClickAction"));
+                            runAction(player, menuFileName, getMenu(menuFileName).getStringList("Menu.ItemList." + item + ".ClickAction"));
                         } else {
-                            runAction(player, meneFileName, DenyActionList);
+                            runAction(player, menuFileName, DenyActionList);
                         }
                     }
                     if (event.getClick() == ClickType.SHIFT_LEFT && event.getClick() == ClickType.SHIFT_RIGHT) {
-                        List<String> DenyActionList = ifAllowClick(player, meneFileName, item, true);
+                        List<String> DenyActionList = ifAllowClick(player, menuFileName, item, true);
                         if (DenyActionList.isEmpty()) {
-                            runAction(player, meneFileName, getMenu(meneFileName).getStringList("Menu.ItemList." + item + ".ShiftClickAction"));
+                            runAction(player, menuFileName, getMenu(menuFileName).getStringList("Menu.ItemList." + item + ".ShiftClickAction"));
                         } else {
-                            runAction(player, meneFileName, DenyActionList);
+                            runAction(player, menuFileName, DenyActionList);
                         }
                     }
                 }
