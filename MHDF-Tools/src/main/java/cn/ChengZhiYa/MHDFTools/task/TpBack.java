19127d6cf1d8365e416bc1c021297a580b36d48f
Dg32z_ <a1410521748@hotmail.com>
2024-06-20 23:24:00 +0800

    删除无意义的类

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/TpBack.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/TpBack.java
@@ -5,8 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Objects;
-
 import static cn.ChengZhiYa.MHDFTools.utils.BCUtil.ServerName;
 import static cn.ChengZhiYa.MHDFTools.utils.BCUtil.TpPlayerTo;
 import static cn.ChengZhiYa.MHDFTools.utils.Util.*;
@@ -14,24 +12,26 @@ import static cn.ChengZhiYa.MHDFTools.utils.Util.*;
 public final class TpBack extends BukkitRunnable {
     @Override
     public void run() {
-        for (Object Key : MapUtil.getIntHasMap().keySet()) {
-            if (Key.toString().contains("_TpBackDelay")) {
-                String PlayerName = Key.toString().replaceAll("_TpBackDelay", "");
-                if (Bukkit.getPlayer(PlayerName) != null) {
-                    Player player = Bukkit.getPlayer(PlayerName);
-                    sendTitle(Objects.requireNonNull(player), i18n("TeleportDelay." + MapUtil.getIntHasMap().get(Key)));
-                    if (MapUtil.getIntHasMap().get(Key) > 0) {
-                        playSound(Objects.requireNonNull(player), sound("TeleportDelay." + MapUtil.getIntHasMap().get(Key)));
-                        MapUtil.getIntHasMap().put(Key, MapUtil.getIntHasMap().get(Key) - 1);
+        for (Object key : MapUtil.getIntHasMap().keySet()) {
+            String keyString = key.toString();
+            if (keyString.contains("_TpBackDelay")) {
+                String playerName = keyString.replaceAll("_TpBackDelay", "");
+                Player player = Bukkit.getPlayer(playerName);
+                if (player != null) {
+                    int delay = MapUtil.getIntHasMap().get(key);
+                    sendTitle(player, i18n("TeleportDelay." + delay));
+                    if (delay > 0) {
+                        playSound(player, sound("TeleportDelay." + delay));
+                        MapUtil.getIntHasMap().put(key, delay - 1);
                     } else {
-                        playSound(Objects.requireNonNull(player), sound("TeleportSound"));
-                        TpPlayerTo(PlayerName, ServerName, MapUtil.getLocationHasMap().get(PlayerName + "_TpBackLocation"));
-                        MapUtil.getLocationHasMap().put(PlayerName + "_UnBackLocation", Objects.requireNonNull(player).getLocation());
+                        playSound(player, sound("TeleportSound"));
+                        TpPlayerTo(playerName, ServerName, MapUtil.getLocationHasMap().get(playerName + "_TpBackLocation"));
+                        MapUtil.getLocationHasMap().put(playerName + "_UnBackLocation", player.getLocation());
                         player.sendMessage(i18n("TpBack.Done"));
-                        MapUtil.getIntHasMap().remove(Key);
+                        MapUtil.getIntHasMap().remove(key);
                     }
                 } else {
-                    MapUtil.getIntHasMap().remove(Key);
+                    MapUtil.getIntHasMap().remove(key);
                 }
             }
         }
