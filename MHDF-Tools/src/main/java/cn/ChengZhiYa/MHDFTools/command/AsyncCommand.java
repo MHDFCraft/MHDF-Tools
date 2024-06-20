6710ae490b5c63acd80c5a53af193c26aa74a430
Dg32z_ <a1410521748@hotmail.com>
2024-06-20 18:39:54 +0800

    优化AsyncCommand类的可读性

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/AsyncCommand.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/AsyncCommand.java
@@ -5,37 +5,31 @@ import cn.ChengZhiYa.MHDFTools.MHDFTools;
 import cn.ChengZhiYa.MHDFTools.task.*;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class AsyncCommand  {
+public class AsyncCommand {
     FileConfiguration getConfig = MHDFTools.instance.getConfig();
     JavaPlugin plugin = MHDFPluginLoader.INSTANCE.getPlugin();
+
     public void start() {
-        if (getConfig.getBoolean("ScoreboardSettings.Enable")) {
-            new Scoreboard().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("LoginSystemSettings.Enable")) {
-            new LoginMessage().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("FlySettings.Enable")) {
-            new Fly().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("BackSettings.Enable")) {
-            new Back().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("TpBackSettings.Enable")) {
-            new TpBack().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("VanishSettings.Enable")) {
-            new Vanish().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("TpaSettings.Enable")) {
-            new TpaTime().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
-        if (getConfig.getBoolean("TpahereSettings.Enable")) {
-            new TpaHereTime().runTaskTimerAsynchronously(plugin, 0L, 20L);
-        }
+        asyncCommand(new Scoreboard(), "ScoreboardSettings.Enable");
+        asyncCommand(new LoginMessage(), "LoginSystemSettings.Enable");
+        asyncCommand(new Fly(), "FlySettings.Enable");
+        asyncCommand(new Back(), "BackSettings.Enable");
+        asyncCommand(new TpBack(), "TpBackSettings.Enable");
+        asyncCommand(new Vanish(), "VanishSettings.Enable");
+        asyncCommand(new TpaTime(), "TpaSettings.Enable");
+        asyncCommand(new TpaHereTime(), "TpahereSettings.Enable");
+
         if (getConfig.getBoolean("TimeMessageSettings.Enable")) {
-            new TimeMessage().runTaskTimerAsynchronously(plugin, 0L, getConfig.getInt("TimeMessageSettings.Delay") * 20L);
+            int delay = getConfig.getInt("TimeMessageSettings.Delay"); //这个是例外 (:
+            new TimeMessage().runTaskTimerAsynchronously(plugin, 0L, delay * 20L);
+        }
+    }
+
+    private void asyncCommand(Runnable task, String configKey) {
+        if (getConfig.getBoolean(configKey)) {
+            ((BukkitRunnable) task).runTaskTimerAsynchronously(plugin, 0L, 20L);
         }
     }
 }
