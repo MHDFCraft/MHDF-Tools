255f0e293184f1240f50d9a33ca59a0714d85758
Dg32z_ <a1410521748@hotmail.com>
2024-06-21 22:57:58 +0800

    整理代码

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/utils/database/HomeUtil.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/utils/database/HomeUtil.java
@@ -201,7 +201,7 @@ public final class HomeUtil {
                 }
                 YamlConfiguration playerHomeData = YamlConfiguration.loadConfiguration(homeDataFile);
 
-                Location homeLocation = new Location(
+                return new Location(
                         Bukkit.getWorld(Objects.requireNonNull(playerHomeData.getString(homeName + ".World"))),
                         playerHomeData.getDouble(homeName + ".X"),
                         playerHomeData.getDouble(homeName + ".Y"),
@@ -209,8 +209,6 @@ public final class HomeUtil {
                         (float) playerHomeData.getDouble(homeName + ".Yaw"),
                         (float) playerHomeData.getDouble(homeName + ".Pitch")
                 );
-
-                return homeLocation;
             } catch (IOException ignored) {
             }
         }
