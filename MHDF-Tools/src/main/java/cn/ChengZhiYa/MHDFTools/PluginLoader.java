8ccd7b1f623619f86bde65a79096a8c18c2249ca
Dg32z_ <a1410521748@hotmail.com>
2024-06-21 20:18:56 +0800

    不需要ProtocolLib了

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/PluginLoader.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/PluginLoader.java
@@ -15,7 +15,6 @@ public enum PluginLoader {
     INSTANCE;
 
     public static boolean hasPlaceholderAPI = true;
-    public static boolean hasProtocolLib = true;
     public static boolean hasVault = true;
     String startDone;
     String stopDone;
