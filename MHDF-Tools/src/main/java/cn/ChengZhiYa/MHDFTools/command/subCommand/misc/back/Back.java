6dcb081f7f328ddbed8b3c3da48e76b9d7a657ba
Dg32z_ <a1410521748@hotmail.com>
2024-06-22 13:53:37 +0800

    修复Back的返回地点错误的问题

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/back/Back.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/back/Back.java
@@ -1,6 +1,7 @@
 package cn.ChengZhiYa.MHDFTools.command.subCommand.misc.back;
 
 import cn.ChengZhiYa.MHDFTools.MHDFTools;
+import cn.ChengZhiYa.MHDFTools.utils.SpigotUtil;
 import cn.ChengZhiYa.MHDFTools.utils.map.MapUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,49 +9,27 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
-import static cn.ChengZhiYa.MHDFTools.utils.SpigotUtil.i18n;
 
 public final class Back implements CommandExecutor {
 
-    private final List<String> disableWorldList;
-
-    public Back() {
-        this.disableWorldList = MHDFTools.instance.getConfig().getStringList("BackSettings.DisableWorldList");
-    }
-
-    @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(i18n("OnlyPlayer"));
-            return false;
-        }
-
-        Player player = (Player) sender;
-        if (MapUtil.getBooleanHashMap().containsKey(player.getName() + "_DeathLocation")) {
-            if (isWorldDisabled(player)) {
-                return false;
+        if (sender instanceof Player) {
+            Player player = (Player)sender;
+            if (MapUtil.getLocationHashMap().get(player.getName() + "_DeathLocation") != null) {
+                if (MHDFTools.instance.getConfig().getStringList("BackSettings.DisableWorldList").contains(MapUtil.getLocationHashMap().get(player.getName() + "_DeathLocation").getWorld().getName()) || MHDFTools.instance.getConfig().getStringList("BackSettings.DisableWorldList").contains(player.getLocation().getWorld().getName())) {
+                    return false;
+                }
+                if (MapUtil.getIntHashMap().get(player.getName() + "_BackDelay") != null || MapUtil.getIntHashMap().get(player.getName() + "_TpBackDelay") != null) {
+                    player.sendMessage(SpigotUtil.i18n("Back.RepectSend"));
+                    return false;
+                }
+                MapUtil.getIntHashMap().put(player.getName() + "_BackDelay", MHDFTools.instance.getConfig().getInt("BackSettings.Delay"));
+            } else {
+                sender.sendMessage(SpigotUtil.i18n("Back.NotFound"));
             }
-            if (isBackDelayActive(player)) {
-                player.sendMessage(i18n("Back.RepectSend"));
-                return false;
-            }
-            MapUtil.getIntHashMap().put(player.getName() + "_BackDelay", MHDFTools.instance.getConfig().getInt("BackSettings.Delay"));
         } else {
-            sender.sendMessage(i18n("Back.NotFound"));
+            sender.sendMessage(SpigotUtil.i18n("OnlyPlayer"));
         }
-
-        return true;
-    }
-
-    private boolean isWorldDisabled(Player player) {
-        String deathWorldName = MapUtil.getLocationHashMap().get(player.getName() + "_DeathLocation").getWorld().getName();
-        String currentWorldName = player.getLocation().getWorld().getName();
-        return disableWorldList.contains(deathWorldName) || disableWorldList.contains(currentWorldName);
-    }
-
-    private boolean isBackDelayActive(Player player) {
-        return MapUtil.getIntHashMap().containsKey(player.getName() + "_BackDelay") || MapUtil.getIntHashMap().containsKey(player.getName() + "_TpBackDelay");
+        return false;
     }
 }
--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/player/PlayerTpBackTask.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/player/PlayerTpBackTask.java
@@ -1,39 +1,36 @@
 package cn.ChengZhiYa.MHDFTools.task.player;
 
+import cn.ChengZhiYa.MHDFTools.utils.BungeeCordUtil;
+import cn.ChengZhiYa.MHDFTools.utils.SpigotUtil;
 import cn.ChengZhiYa.MHDFTools.utils.map.MapUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import static cn.ChengZhiYa.MHDFTools.utils.BungeeCordUtil.ServerName;
-import static cn.ChengZhiYa.MHDFTools.utils.BungeeCordUtil.TpPlayerTo;
-import static cn.ChengZhiYa.MHDFTools.utils.SpigotUtil.*;
+import java.util.Objects;
 
 public final class PlayerTpBackTask extends BukkitRunnable {
     @Override
     public void run() {
-        for (Object key : MapUtil.getIntHashMap().keySet()) {
-            String keyString = key.toString();
-            if (keyString.contains("_TpBackDelay")) {
-                String playerName = keyString.replaceAll("_TpBackDelay", "");
-                Player player = Bukkit.getPlayer(playerName);
-                if (player != null) {
-                    int delay = MapUtil.getIntHashMap().get(key);
-                    sendTitle(player, i18n("TeleportDelay." + delay));
-                    if (delay > 0) {
-                        playSound(player, sound("TeleportDelay." + delay));
-                        MapUtil.getIntHashMap().put(key, delay - 1);
-                    } else {
-                        playSound(player, sound("TeleportSound"));
-                        TpPlayerTo(playerName, ServerName, MapUtil.getLocationHashMap().get(playerName + "_TpBackLocation"));
-                        MapUtil.getLocationHashMap().put(playerName + "_UnBackLocation", player.getLocation());
-                        player.sendMessage(i18n("TpBack.Done"));
-                        MapUtil.getIntHashMap().remove(key);
-                    }
-                } else {
-                    MapUtil.getIntHashMap().remove(key);
+        for (Object Key : MapUtil.getIntHashMap().keySet()) {
+            if (!Key.toString().contains("_TpBackDelay")) continue;
+            String PlayerName = Key.toString().replaceAll("_TpBackDelay", "");
+            if (Bukkit.getPlayer(PlayerName) != null) {
+                Player player = Bukkit.getPlayer(PlayerName);
+                SpigotUtil.sendTitle(Objects.requireNonNull(player), SpigotUtil.i18n("TeleportDelay." + MapUtil.getIntHashMap().get(Key)));
+                if (MapUtil.getIntHashMap().get(Key) > 0) {
+                    SpigotUtil.playSound(Objects.requireNonNull(player), SpigotUtil.sound("TeleportDelay." + MapUtil.getIntHashMap().get(Key)));
+                    MapUtil.getIntHashMap().put(Key, MapUtil.getIntHashMap().get(Key) - 1);
+                    continue;
                 }
+                SpigotUtil.playSound(Objects.requireNonNull(player), SpigotUtil.sound("TeleportSound"));
+                BungeeCordUtil.TpPlayerTo(PlayerName, BungeeCordUtil.ServerName, MapUtil.getLocationHashMap().get(PlayerName + "_TpBackLocation"));
+                MapUtil.getLocationHashMap().put(PlayerName + "_UnBackLocation", Objects.requireNonNull(player).getLocation());
+                player.sendMessage(SpigotUtil.i18n("TpBack.Done"));
+                MapUtil.getIntHashMap().remove(Key);
+                continue;
             }
+            MapUtil.getIntHashMap().remove(Key);
         }
     }
 }
