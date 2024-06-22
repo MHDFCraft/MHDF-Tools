9c6431895fbda2f985e3c282c5abaad18c8aea9e
Dg32z_ <a1410521748@hotmail.com>
2024-06-22 21:00:19 +0800

    NONE

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/player/PlayerTpaTask.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/player/PlayerTpaTask.java
@@ -10,28 +10,28 @@ public final class PlayerTpaTask extends BukkitRunnable {
     @Override
     public void run() {
         if (MHDFTools.instance.getConfig().getBoolean("TpaSettings.Enable")) {
-                for (Object Tpa : MapUtil.getStringHashMap().keySet()) {
-                    if (!Tpa.toString().contains("_TPAPlayerName")) continue;
-                    String PlayerName = Tpa.toString().replaceAll("_TPAPlayerName", "");
-                    String TagerPlayerName = MapUtil.getStringHashMap().get(Tpa);
-                    int Time = MapUtil.getIntHashMap().get(PlayerName + "_TPATime");
-                    if (BungeeCordUtil.ifPlayerOnline(PlayerName)) {
-                        if (BungeeCordUtil.ifPlayerOnline(TagerPlayerName)) {
-                            if (Time >= 0) {
-                                MapUtil.getIntHashMap().put(PlayerName + "_TPATime", Time - 1);
-                                continue;
-                            }
-                            BungeeCordUtil.SendMessage(PlayerName, SpigotUtil.i18n("Tpa.TimeOutDone", TagerPlayerName));
-                            BungeeCordUtil.SendMessage(TagerPlayerName, SpigotUtil.i18n("Tpa.TimeOut", PlayerName));
-                            BungeeCordUtil.CancelTpa(PlayerName);
+            for (Object Tpa : MapUtil.getStringHashMap().keySet()) {
+                if (!Tpa.toString().contains("_TPAPlayerName")) continue;
+                String PlayerName = Tpa.toString().replaceAll("_TPAPlayerName", "");
+                String TagerPlayerName = MapUtil.getStringHashMap().get(Tpa);
+                int Time = MapUtil.getIntHashMap().get(PlayerName + "_TPATime");
+                if (BungeeCordUtil.ifPlayerOnline(PlayerName)) {
+                    if (BungeeCordUtil.ifPlayerOnline(TagerPlayerName)) {
+                        if (Time >= 0) {
+                            MapUtil.getIntHashMap().put(PlayerName + "_TPATime", Time - 1);
                             continue;
                         }
-                        BungeeCordUtil.SendMessage(PlayerName, SpigotUtil.i18n("Tpa.Offline", TagerPlayerName));
+                        BungeeCordUtil.SendMessage(PlayerName, SpigotUtil.i18n("Tpa.TimeOutDone", TagerPlayerName));
+                        BungeeCordUtil.SendMessage(TagerPlayerName, SpigotUtil.i18n("Tpa.TimeOut", PlayerName));
                         BungeeCordUtil.CancelTpa(PlayerName);
                         continue;
                     }
+                    BungeeCordUtil.SendMessage(PlayerName, SpigotUtil.i18n("Tpa.Offline", TagerPlayerName));
                     BungeeCordUtil.CancelTpa(PlayerName);
+                    continue;
                 }
+                BungeeCordUtil.CancelTpa(PlayerName);
             }
         }
     }
+}
