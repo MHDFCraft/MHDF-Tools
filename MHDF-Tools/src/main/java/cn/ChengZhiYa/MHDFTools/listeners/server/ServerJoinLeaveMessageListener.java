71b6c2105afd2c9ac92e6c53e26d2274487ce13c
Dg32z_ <a1410521748@hotmail.com>
2024-06-24 18:30:28 +0800

    NONE

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/listeners/server/ServerJoinLeaveMessageListener.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/listeners/server/ServerJoinLeaveMessageListener.java
@@ -1,19 +1,21 @@
 package cn.ChengZhiYa.MHDFTools.listeners.server;
 
-import cn.ChengZhiYa.MHDFTools.MHDFTools;
+import cn.ChengZhiYa.MHDFTools.PluginLoader;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import static cn.ChengZhiYa.MHDFTools.utils.SpigotUtil.getJoinMessage;
 import static cn.ChengZhiYa.MHDFTools.utils.SpigotUtil.getQuitMessage;
 
 public final class ServerJoinLeaveMessageListener implements Listener {
+    JavaPlugin plugin = PluginLoader.INSTANCE.getPlugin();
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
-        if (MHDFTools.instance.getConfig().getBoolean("CustomJoinServerMessageSettings.Enable")) {
+        if (plugin.getConfig().getBoolean("CustomJoinServerMessageSettings.Enable")) {
             Player player = event.getPlayer();
             if (player.hasPermission("MHDFTools.JoinMessage")) {
                 event.setJoinMessage(getJoinMessage(player));
@@ -23,7 +25,7 @@ public final class ServerJoinLeaveMessageListener implements Listener {
 
     @EventHandler
     public void onPlayerQuitEvent(PlayerQuitEvent event) {
-        if (MHDFTools.instance.getConfig().getBoolean("CustomQuitServerMessageSettings.Enable")) {
+        if (plugin.getConfig().getBoolean("CustomQuitServerMessageSettings.Enable")) {
             Player player = event.getPlayer();
             if (player.hasPermission("MHDFTools.QuitMessage")) {
                 event.setQuitMessage(getQuitMessage(player));
