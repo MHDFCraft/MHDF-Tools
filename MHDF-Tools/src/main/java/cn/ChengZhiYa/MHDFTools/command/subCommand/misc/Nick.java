2fb4fbe3b8c7fe8d7ce6f867163cd931f18a79dc
白神遥真可爱 <292200693@qq.com>
2024-08-06 19:37:56 +0800

    升级Nick

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/Nick.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/Nick.java
@@ -3,6 +3,7 @@ package cn.ChengZhiYa.MHDFTools.command.subCommand.misc;
 import cn.ChengZhiYa.MHDFTools.MHDFTools;
 import cn.ChengZhiYa.MHDFTools.utils.BungeeCordUtil;
 import cn.ChengZhiYa.MHDFTools.utils.database.NickUtil;
+import cn.ChengZhiYa.MHDFTools.utils.message.LogUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -23,10 +24,11 @@ public final class Nick implements TabExecutor {
                 if (sender instanceof Player) {
                     Player player = (Player) sender;
                     boolean setNick = !args[0].equals("off");
-                    String nickName = setNick ? player.getName() : args[0];
-                    if (player.hasPermission("MHDFTools.Command.Nick.BypassCheck") && setNick) {
+                    String nickName = setNick ? args[0] : player.getName();
+                    if (!player.hasPermission("MHDFTools.Command.Nick.BypassCheck") && setNick) {
                         for (String blackWord : MHDFTools.instance.getConfig().getStringList("NickSettings.BlackWordList")) {
                             if (nickName.toLowerCase(Locale.ROOT).contains(blackWord.toLowerCase(Locale.ROOT))) {
+                                LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "敏感词: " + blackWord, "原因: 不能包含敏感词");
                                 player.sendMessage(i18n("Nick.BlackWord", blackWord));
                                 return false;
                             }
@@ -34,23 +36,28 @@ public final class Nick implements TabExecutor {
                         if (MHDFTools.instance.getConfig().getBoolean("NickSettings.AntiSetPlayerName")) {
                             for (OfflinePlayer players : Bukkit.getOfflinePlayers()) {
                                 if (Objects.equals(players.getName(), nickName)) {
+                                    LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "其他玩家的昵称: " + nickName, "原因: 不能使用其他玩家的ID");
                                     player.sendMessage(i18n("Nick.AntiSetPlayerName"));
-                                 return false;
+                                    return false;
                                 }
                             }
                         }
                         if (nickName.length() < MHDFTools.instance.getConfig().getInt("NickSettings.MinLength")) {
+                            LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "最小长度: " + MHDFTools.instance.getConfig().getInt("NickSettings.MinLength"), "原因: 名字过短");
                             player.sendMessage(i18n("Nick.MinLength", String.valueOf(MHDFTools.instance.getConfig().getInt("NickSettings.MinLength"))));
                             return false;
                         }
                         if (nickName.length() > MHDFTools.instance.getConfig().getInt("NickSettings.MaxLength")) {
+                            LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "最长长度: " + MHDFTools.instance.getConfig().getInt("NickSettings.MinLength"), "原因: 名字过长");
                             player.sendMessage(i18n("Nick.MaxLength", String.valueOf(MHDFTools.instance.getConfig().getInt("NickSettings.MaxLength"))));
                             return false;
                         }
                     }
                     if (setNick) {
+                        LogUtil.debug("设置昵称成功", "设定昵称: " + nickName, "玩家ID: " + player.getName());
                         NickUtil.setNickName(player.getName(), nickName);
                     } else {
+                        LogUtil.debug("移除昵称成功", "设定昵称: " + nickName, "玩家ID: " + player.getName());
                         NickUtil.removeNickName(player.getName());
                     }
                     player.setDisplayName(nickName);
@@ -68,8 +75,10 @@ public final class Nick implements TabExecutor {
                     boolean setNick = !args[0].equals("off");
                     String nickName = setNick ? args[1] : args[0];
                     if (setNick) {
+                        LogUtil.debug("设置昵称成功", "设定昵称: " + nickName, "玩家ID: " + args[1]);
                         NickUtil.setNickName(args[1], nickName);
                     } else {
+                        LogUtil.debug("移除昵称成功", "设定昵称: " + nickName, "玩家ID: " + args[1]);
                         NickUtil.removeNickName(args[1]);
                     }
                     if (Bukkit.getPlayer(args[1]) != null) {
