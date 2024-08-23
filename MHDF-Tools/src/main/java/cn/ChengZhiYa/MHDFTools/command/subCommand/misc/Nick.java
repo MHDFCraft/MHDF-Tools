5809eeb51493e7ae9e72d0b21d3521d123e95dbb
Dg32z_ <a1410521748@hotmail.com>
2024-08-23 14:43:01 +0800

    异步设置nick

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/Nick.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/Nick.java
@@ -3,7 +3,7 @@ package cn.ChengZhiYa.MHDFTools.command.subCommand.misc;
 import cn.ChengZhiYa.MHDFTools.PluginLoader;
 import cn.ChengZhiYa.MHDFTools.utils.BungeeCordUtil;
 import cn.ChengZhiYa.MHDFTools.utils.database.NickUtil;
-import cn.ChengZhiYa.MHDFTools.utils.message.LogUtil;
+import cn.ChengZhiYa.MHDFTools.utils.message.ColorLogs;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -28,7 +28,7 @@ public final class Nick implements TabExecutor {
                     if (!player.hasPermission("MHDFTools.Command.Nick.BypassCheck") && setNick) {
                         for (String blackWord : PluginLoader.INSTANCE.getPlugin().getConfig().getStringList("NickSettings.BlackWordList")) {
                             if (nickName.toLowerCase(Locale.ROOT).contains(blackWord.toLowerCase(Locale.ROOT))) {
-                                LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "敏感词: " + blackWord, "原因: 不能包含敏感词");
+                                ColorLogs.debug("设置昵称失败", "设定昵称: " + nickName, "敏感词: " + blackWord, "原因: 不能包含敏感词");
                                 player.sendMessage(i18n("Nick.BlackWord", blackWord));
                                 return false;
                             }
@@ -36,34 +36,31 @@ public final class Nick implements TabExecutor {
                         if (PluginLoader.INSTANCE.getPlugin().getConfig().getBoolean("NickSettings.AntiSetPlayerName")) {
                             for (OfflinePlayer players : Bukkit.getOfflinePlayers()) {
                                 if (Objects.equals(players.getName(), nickName)) {
-                                    LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "其他玩家的昵称: " + nickName, "原因: 不能使用其他玩家的ID");
+                                    ColorLogs.debug("设置昵称失败", "设定昵称: " + nickName, "其他玩家的昵称: " + nickName, "原因: 不能使用其他玩家的ID");
                                     player.sendMessage(i18n("Nick.AntiSetPlayerName"));
                                     return false;
                                 }
                             }
                         }
                         if (nickName.length() < PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MinLength")) {
-                            LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "最小长度: " + PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MinLength"), "原因: 名字过短");
+                            ColorLogs.debug("设置昵称失败", "设定昵称: " + nickName, "最小长度: " + PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MinLength"), "原因: 名字过短");
                             player.sendMessage(i18n("Nick.MinLength", String.valueOf(PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MinLength"))));
                             return false;
                         }
                         if (nickName.length() > PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MaxLength")) {
-                            LogUtil.debug("设置昵称失败", "设定昵称: " + nickName, "最长长度: " + PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MinLength"), "原因: 名字过长");
+                            ColorLogs.debug("设置昵称失败", "设定昵称: " + nickName, "最长长度: " + PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MinLength"), "原因: 名字过长");
                             player.sendMessage(i18n("Nick.MaxLength", String.valueOf(PluginLoader.INSTANCE.getPlugin().getConfig().getInt("NickSettings.MaxLength"))));
                             return false;
                         }
                     }
                     if (setNick) {
-                        LogUtil.debug("设置昵称成功", "设定昵称: " + nickName, "玩家ID: " + player.getName());
+                        ColorLogs.debug("设置昵称成功", "设定昵称: " + nickName, "玩家ID: " + player.getName());
                         NickUtil.setNickName(player.getName(), nickName);
                     } else {
-                        LogUtil.debug("移除昵称成功", "设定昵称: " + nickName, "玩家ID: " + player.getName());
+                        ColorLogs.debug("移除昵称成功", "设定昵称: " + nickName, "玩家ID: " + player.getName());
                         NickUtil.removeNickName(player.getName());
                     }
-                    player.setDisplayName(nickName);
-                    player.setCustomName(nickName);
-                    player.setPlayerListName(nickName);
-                    player.setCustomNameVisible(!setNick);
+                    setNickForPlayer(player, nickName, args);
                     player.sendMessage(i18n("Nick.SetDone", nickName));
                 } else {
                     sender.sendMessage(i18n("OnlyPlayer"));
@@ -75,18 +72,15 @@ public final class Nick implements TabExecutor {
                     boolean setNick = !args[0].equals("off");
                     String nickName = setNick ? args[1] : args[0];
                     if (setNick) {
-                        LogUtil.debug("设置昵称成功", "设定昵称: " + nickName, "玩家ID: " + args[1]);
+                        ColorLogs.debug("设置昵称成功", "设定昵称: " + nickName, "玩家ID: " + args[1]);
                         NickUtil.setNickName(args[1], nickName);
                     } else {
-                        LogUtil.debug("移除昵称成功", "设定昵称: " + nickName, "玩家ID: " + args[1]);
+                        ColorLogs.debug("移除昵称成功", "设定昵称: " + nickName, "玩家ID: " + args[1]);
                         NickUtil.removeNickName(args[1]);
                     }
                     if (Bukkit.getPlayer(args[1]) != null) {
                         Player player = (Player) sender;
-                        player.setDisplayName(nickName);
-                        player.setCustomName(nickName);
-                        player.setPlayerListName(nickName);
-                        player.setCustomNameVisible(!setNick);
+                        setNickForPlayer(player, nickName, args);
                         player.sendMessage(i18n("Nick.SetDone", nickName));
                     }
                     sender.sendMessage(i18n("Nick.SetOtherDone", args[1], nickName));
@@ -100,6 +94,15 @@ public final class Nick implements TabExecutor {
                 break;
         }
         return true;
+    }
+    private void setNickForPlayer(Player player, String name, String[] args) {
+        boolean setNick = !args[0].equals("off");
+        Bukkit.getScheduler().runTaskAsynchronously(PluginLoader.INSTANCE.getPlugin(), () -> {
+            player.setDisplayName(name);
+            player.setCustomName(name);
+            player.setPlayerListName(name);
+            player.setCustomNameVisible(!setNick);
+        });
     }
 
     @Override
