ad7dfc86ea7b1fd80ff4da5c816cdd07488479ca
Dg32z_ <a1410521748@hotmail.com>
2024-08-23 18:20:43 +0800

    修复Say命令错误的args长度判断

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/Say.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/command/subCommand/misc/Say.java
@@ -6,19 +6,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 import static cn.ChengZhiYa.MHDFTools.utils.SpigotUtil.Placeholder;
 import static cn.ChengZhiYa.MHDFTools.utils.SpigotUtil.i18n;
 
 public final class Say implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (args.length >= 1) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (String arg : args) {
-                stringBuilder.append(arg).append(" ");
-            }
-            Bukkit.broadcast(Placeholder(null,stringBuilder.toString().replaceAll(args[0],"")),"MHDFTools.Broadcast");
-        }else {
+        if (args.length > 0) {
+            String message = args.length == 1 ? args[0] : String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+            Bukkit.broadcast(Placeholder(null, message), "MHDFTools.Broadcast");
+        } else {
             sender.sendMessage(i18n("Usage.Say"));
         }
         return false;
