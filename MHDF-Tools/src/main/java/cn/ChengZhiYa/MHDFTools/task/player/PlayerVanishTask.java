a03f06274a02dd5c7c9ca436c536282d9db3914e
Dg32z_ <a1410521748@hotmail.com>
2024-08-26 17:31:19 +0800

    把vanish执行那部分改成FoliaScheduler

--- MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/player/PlayerVanishTask.java
+++ MHDF-Tools/src/main/java/cn/ChengZhiYa/MHDFTools/task/player/PlayerVanishTask.java
@@ -2,31 +2,58 @@ package cn.ChengZhiYa.MHDFTools.task.player;
 
 import cn.ChengZhiYa.MHDFTools.PluginLoader;
 import cn.ChengZhiYa.MHDFTools.utils.database.VanishUtil;
+import com.github.Anon8281.universalScheduler.foliaScheduler.FoliaScheduler;
 import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.lang.reflect.Method;
 import java.util.function.Consumer;
 
 public final class PlayerVanishTask implements Consumer<ScheduledTask> {
 
+    private static final Method HIDE_PLAYER_METHOD;
+
+    static {
+        Method method;
+        try {
+            method = Player.class.getDeclaredMethod("hidePlayer", Plugin.class, Player.class);
+        } catch (NoSuchMethodException e) {
+            method = null;
+        }
+        HIDE_PLAYER_METHOD = method;
+    }
+
     @Override
     public void accept(ScheduledTask task) {
         if (PluginLoader.INSTANCE.getPlugin().getConfig().getBoolean("VanishSettings.Enable")) {
-            for (String vanishPlayer : VanishUtil.getVanishList()) {
-                Player player = Bukkit.getPlayerExact(vanishPlayer);
-                if (player != null) {
+            for (String vanishPlayerName : VanishUtil.getVanishList()) {
+                Player vanishPlayer = Bukkit.getPlayerExact(vanishPlayerName);
+                if (vanishPlayer != null) {
                     for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                        try {
-                            Player.class.getDeclaredMethod("hidePlayer", Plugin.class, Player.class);
-                            Bukkit.getRegionScheduler().run(PluginLoader.INSTANCE.getPlugin(), onlinePlayer.getLocation(), t -> onlinePlayer.hidePlayer(PluginLoader.INSTANCE.getPlugin(), player));
-                        } catch (NoSuchMethodException e) {
-                            Bukkit.getRegionScheduler().run(PluginLoader.INSTANCE.getPlugin(), onlinePlayer.getLocation(), t -> onlinePlayer.hidePlayer(player));
-                        }
+                        hidePlayer(onlinePlayer, vanishPlayer);
                     }
                 }
             }
+        }
+    }
+
+    private void hidePlayer(Player onlinePlayer, Player vanishPlayer) {
+        if (HIDE_PLAYER_METHOD != null) {
+            try {
+                new FoliaScheduler(PluginLoader.INSTANCE.getPlugin()).runTask(() -> {
+                    try {
+                        HIDE_PLAYER_METHOD.invoke(onlinePlayer, PluginLoader.INSTANCE.getPlugin(), vanishPlayer);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Bukkit.getScheduler().runTask(PluginLoader.INSTANCE.getPlugin(), () -> onlinePlayer.hidePlayer(vanishPlayer));
         }
     }
 }
